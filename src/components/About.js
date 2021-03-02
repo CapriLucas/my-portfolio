f87bad9619283f40a9324be66cbc8d9af0d0cd98
CapriLucas <capriatalucas@gmail.com>
2021-03-02 18:22:32 -0300

    changed about page

--- src/components/About.js
+++ src/components/About.js
@@ -19,17 +19,17 @@ export default function About() {
     if (!author) return <div>Loading...</div>;
 
     return (
-        <main className="relative">
-            <img src={fondo} alt="Fondo" className="absolute w-full" />
-            <div className="p-10 lg:pt-48 container mx-auto relative">
-                <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
-                    <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name} />
+        <main className="bg-green-100 min-h-screen p-12">
+
+            <div className="p-10 lg:pt-20 container mx-auto relative">
+                <section className="bg-gray-50 rounded-lg shadow-2xl lg:flex p-8">
+                    <img src={urlFor(author.authorImage).url()} className="rounded w-auto h-96 lg:w-auto lg:h-96 mr-8" alt={author.name} />
                     <div className="text-lg flex flex-col justify-center">
                         <h1 className="cursive text-6xl text-green-300 mb-4">
                             Hey there. I'm{" "}
-                            <span className="text-green-100">{author.name}</span>
+                            <span className="text-green-500">{author.name}</span>
                         </h1>
-                        <div className="prose lg:prose-xl text-white">
+                        <div className="prose lg:prose-xl ">
                             <BlockContent blocks={author.bio} projectId="0bnmee57" dataset="production" />
                         </div>
                     </div>
