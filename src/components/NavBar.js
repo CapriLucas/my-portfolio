dca7c2df78e72be76fee82916d2a06560c066d50
CapriLucas <capriatalucas@gmail.com>
2021-03-03 12:57:53 -0300

    fixed navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -61,14 +61,14 @@ export default function NavBar() {
     else {
         // For smartphones
         return (
-            <header className="bg-red-600">
+            <header className="headerNav">
                 <div className="container mx-auto flex justify-between">
                     <nav className="flex">
                         <NavLink
                             to="/"
                             exact
                             activeClassName="text-white"
-                            className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+                            className="inline-flex items-center py-6 px-3 mr-4 text-red-900 hover:text-yellow-500 text-5xl font-bold cursive tracking-widest"
                         >
                             CapriLucas
                         </NavLink>
